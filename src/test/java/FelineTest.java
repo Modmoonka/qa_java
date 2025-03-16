e2d1fb726453444bbac15a62450f8907e4b79f41
BabatenkoTO <tanya-mongush@mail.ru>
2025-03-16 12:28:28 +0700

    add new change

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
